7cbf7888bcdea07118fdb696be7108fbf137fe4f
carraes <carraeshb@gmail.com>
2023-02-26 20:20:22 -0300

    feat: PersonRepository, initiate DBCRUD

---
+++ src/main/java/com/arraes/peopledbweb/data/PersonRepository.java
@@ -0,0 +1,8 @@
+package com.arraes.peopledbweb.data;
+
+import com.arraes.peopledbweb.biz.model.Person;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends CrudRepository<Person, Long> {}
