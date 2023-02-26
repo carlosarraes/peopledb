f8499c155d358e8a0d65a5d662cfae2096b1e102
carraes <carraeshb@gmail.com>
2023-02-26 20:20:44 -0300

    feat: load Data/Initiate data if no data

---
+++ src/main/java/com/arraes/peopledbweb/data/PersonDataLoader.java
@@ -0,0 +1,33 @@
+package com.arraes.peopledbweb.data;
+
+import com.arraes.peopledbweb.biz.model.Person;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonDataLoader implements ApplicationRunner {
+  private PersonRepository personRepository;
+
+  public PersonDataLoader(PersonRepository personRepository) {
+    this.personRepository = personRepository;
+  }
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    if (personRepository.count() == 0) {
+      List<Person> people =
+          List.of(
+              new Person(null, "Pete", "Snake", LocalDate.of(1950, 1, 1), new BigDecimal(1000)),
+              new Person(null, "Jennifer", "Smith", LocalDate.of(1960, 1, 1), new BigDecimal(2000)),
+              new Person(null, "Vishnu", "Jackson", LocalDate.of(1970, 1, 1), new BigDecimal(3000)),
+              new Person(null, "Alice", "Kim", LocalDate.of(1980, 1, 1), new BigDecimal(4000)),
+              new Person(null, "Akira", "McGuire", LocalDate.of(1990, 1, 1), new BigDecimal(5000)));
+
+      personRepository.saveAll(people);
+    }
+  }
+}
