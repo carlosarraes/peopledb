e630544cadfab0b4501e02c802c52c356db33b55
carraes <carraeshb@gmail.com>
2023-02-26 20:19:57 -0300

    refactor: PErson model, id/noargs/entity

--- src/main/java/com/arraes/peopledbweb/biz/model/Person.java
+++ src/main/java/com/arraes/peopledbweb/biz/model/Person.java
@@ -2,13 +2,19 @@ package com.arraes.peopledbweb.biz.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Person {
-  private Long id;
+  @Id @GeneratedValue private Long id;
   private String firstName;
   private String lastName;
   private LocalDate dob;
