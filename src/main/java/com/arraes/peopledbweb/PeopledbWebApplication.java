9a92bd85d53c1058dccacdf1505258e2ff68fd65
carraes <carraeshb@gmail.com>
2023-02-26 08:34:48 -0300

    chore: initial commit

---
+++ src/main/java/com/arraes/peopledbweb/PeopledbWebApplication.java
@@ -0,0 +1,12 @@
+package com.arraes.peopledbweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopledbWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PeopledbWebApplication.class, args);
+  }
+}
