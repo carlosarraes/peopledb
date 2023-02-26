3a81448c1d4cf1c6bc7202ca7d910dcc359815a9
carraes <carraeshb@gmail.com>
2023-02-26 14:42:26 -0300

    feat: Person model, using lombok;

---
+++ src/main/java/com/arraes/peopledbweb/biz/model/Person.java
@@ -0,0 +1,16 @@
+package com.arraes.peopledbweb.biz.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Person {
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private LocalDate dob;
+  private BigDecimal salary;
+}
