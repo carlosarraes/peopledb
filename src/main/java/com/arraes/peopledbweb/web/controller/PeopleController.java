d9b6ebe720b424f84bfa539cb23da57cf79d1bbb
carraes <carraeshb@gmail.com>
2023-02-26 20:20:58 -0300

    refactor: get data from DB instead of hardcoded

--- src/main/java/com/arraes/peopledbweb/web/controller/PeopleController.java
+++ src/main/java/com/arraes/peopledbweb/web/controller/PeopleController.java
@@ -1,28 +1,28 @@
 package com.arraes.peopledbweb.web.controller;
 
 import com.arraes.peopledbweb.biz.model.Person;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
+import com.arraes.peopledbweb.data.PersonRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
+  private PersonRepository personRepository;
+
+  public PeopleController(PersonRepository personRepository) {
+    this.personRepository = personRepository;
+  }
+
+  @ModelAttribute("people")
+  public Iterable<Person> getPeople() {
+    return personRepository.findAll();
+  }
 
   @GetMapping
-  public String getPeople(Model model) {
-    List<Person> people =
-        List.of(
-            new Person(10l, "Jake", "Snake", LocalDate.of(1950, 1, 1), new BigDecimal(1000)),
-            new Person(20l, "Sarah", "Smith", LocalDate.of(1960, 1, 1), new BigDecimal(2000)),
-            new Person(30l, "Johnny", "Jackson", LocalDate.of(1970, 1, 1), new BigDecimal(3000)),
-            new Person(40l, "Bobby", "Kim", LocalDate.of(1980, 1, 1), new BigDecimal(4000)),
-            new Person(50l, "Tane", "McGuire", LocalDate.of(1990, 1, 1), new BigDecimal(5000)));
-    model.addAttribute("people", people);
+  public String showPeoplePage() {
     return "people";
   }
 }
